f96306a354470ef424fe8e18aa56353c010f6e5f
Kwameoduro <odurokwameee@gmail.com>
2024-06-03 17:52:20 +0000

    add: test for gradient descent

---
+++ test_for_problem3.py
@@ -0,0 +1,14 @@
+initial_w = 0
+initial_b = 0
+
+tmp_dj_dw, tmp_dj_db = compute_gradient(x_train, y_train, initial_w, initial_b)
+print('Gradient at initial w, b (zeros):', tmp_dj_dw, tmp_dj_db)
+
+compute_gradient_test(compute_gradient)
+
+
+test_w = 0.2
+test_b = 0.2
+tmp_dj_dw, tmp_dj_db = compute_gradient(x_train, y_train, test_w, test_b)
+
+print('Gradient at test w, b:', tmp_dj_dw, tmp_dj_db)
