db6df2bb0b55ed05b3986eb9efe5d56c236cf60f
Kwameoduro <odurokwameee@gmail.com>
2024-06-06 13:16:23 +0000

    update: plotting the results of gradient descent

--- problem12.py
+++ problem12.py
@@ -53,3 +53,83 @@ b_tmp = 1.
 dj_db_tmp, dj_dw_tmp = compute_gradient_logistic(X_tmp, y_tmp, w_tmp, b_tmp)
 print(f"dj_db: {dj_db_tmp}" )
 print(f"dj_dw: {dj_dw_tmp.tolist()}" )
+
+
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters):
+    """
+    Performs batch gradient descent
+
+    Args:
+      X (ndarray (m,n)   : Data, m examples with n features
+      y (ndarray (m,))   : target values
+      w_in (ndarray (n,)): Initial values of model parameters
+      b_in (scalar)      : Initial values of model parameter
+      alpha (float)      : Learning rate
+      num_iters (scalar) : number of iterations to run gradient descent
+
+    Returns:
+      w (ndarray (n,))   : Updated values of parameters
+      b (scalar)         : Updated value of parameter
+    """
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    w = copy.deepcopy(w_in)  # avoid modifying global w within function
+    b = b_in
+
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters
+        dj_db, dj_dw = compute_gradient_logistic(X, y, w, b)
+
+        # Update Parameters using w, b, alpha and gradient
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
+        # Save cost J at each iteration
+        if i < 100000:  # prevent resource exhaustion
+            J_history.append(compute_cost_logistic(X, y, w, b))
+
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i % math.ceil(num_iters / 10) == 0:
+            print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
+
+    return w, b, J_history  # return final w,b and J history for graphing
+
+w_tmp  = np.zeros_like(X_train[0])
+b_tmp  = 0.
+alph = 0.1
+iters = 10000
+
+w_out, b_out, _ = gradient_descent(X_train, y_train, w_tmp, b_tmp, alph, iters)
+print(f"\nupdated parameters: w:{w_out}, b:{b_out}")
+
+"""
+output:
+
+Iteration    0: Cost 0.684610468560574   
+Iteration 1000: Cost 0.1590977666870456   
+Iteration 2000: Cost 0.08460064176930081   
+Iteration 3000: Cost 0.05705327279402531   
+Iteration 4000: Cost 0.042907594216820076   
+Iteration 5000: Cost 0.034338477298845684   
+Iteration 6000: Cost 0.028603798022120097   
+Iteration 7000: Cost 0.024501569608793   
+Iteration 8000: Cost 0.02142370332569295   
+Iteration 9000: Cost 0.019030137124109114   
+
+updated parameters: w:[5.28 5.08], b:-14.222409982019837
+"""
+fig,ax = plt.subplots(1,1,figsize=(5,4))
+# plot the probability
+plt_prob(ax, w_out, b_out)
+
+# Plot the original data
+ax.set_ylabel(r'$x_1$')
+ax.set_xlabel(r'$x_0$')
+ax.axis([0, 4, 0, 3.5])
+plot_data(X_train,y_train,ax)
+
+# Plot the decision boundary
+x0 = -b_out/w_out[0]
+x1 = -b_out/w_out[1]
+ax.plot([0,x0],[x1,0], c=dlc["dlblue"], lw=1)
+plt.show()
