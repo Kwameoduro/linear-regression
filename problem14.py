b4515f61b0a28db361dbb349e42c35fd2f955c80
Kwameoduro <odurokwameee@gmail.com>
2024-06-09 14:02:22 +0000

    update: Cost function for regularized logistic regression

--- problem14.py
+++ problem14.py
@@ -48,3 +48,48 @@ print("Regularized cost:", cost_tmp)
 
 # output: Regularized cost: 0.07917239320214275
 
+def compute_cost_logistic_reg(X, y, w, b, lambda_=1):
+    """
+    Computes the cost over all examples
+    Args:
+    Args:
+      X (ndarray (m,n): Data, m examples with n features
+      y (ndarray (m,)): target values
+      w (ndarray (n,)): model parameters
+      b (scalar)      : model parameter
+      lambda_ (scalar): Controls amount of regularization
+    Returns:
+      total_cost (scalar):  cost
+    """
+
+    m, n = X.shape
+    cost = 0.
+    for i in range(m):
+        z_i = np.dot(X[i], w) + b  # (n,)(n,)=scalar, see np.dot
+        f_wb_i = sigmoid(z_i)  # scalar
+        cost += -y[i] * np.log(f_wb_i) - (1 - y[i]) * np.log(1 - f_wb_i)  # scalar
+
+    cost = cost / m  # scalar
+
+    reg_cost = 0
+    for j in range(n):
+        reg_cost += (w[j] ** 2)  # scalar
+    reg_cost = (lambda_ / (2 * m)) * reg_cost  # scalar
+
+    total_cost = cost + reg_cost  # scalar
+    return total_cost
+
+
+np.random.seed(1)
+X_tmp = np.random.rand(5,6)
+y_tmp = np.array([0,1,0,1,0])
+w_tmp = np.random.rand(X_tmp.shape[1]).reshape(-1,)-0.5
+b_tmp = 0.5
+lambda_tmp = 0.7
+cost_tmp = compute_cost_logistic_reg(X_tmp, y_tmp, w_tmp, b_tmp, lambda_tmp)
+
+print("Regularized cost:", cost_tmp)
+
+
+# output: Regularized cost: 0.6850849138741673
+
